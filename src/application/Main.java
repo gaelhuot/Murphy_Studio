99cdef03e47435419b0bebf6a85eb0d793b6bf23
ThibaudJeannin <thibaud.jeannin@gmail.com>
2017-12-28 00:54:35 +0100

    Ajoute exemple d'interface split simple

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,9 +2,14 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -23,6 +28,19 @@ public class Main extends Application {
         primaryStage.setX((bounds.getWidth() - primaryStage.getWidth()) / 2);
         primaryStage.setY((bounds.getHeight() - primaryStage.getHeight()) / 4);
         primaryStage.show();
+
+        BorderPane workspace = (BorderPane) scene.lookup("#workspace_pane");
+        SplitPane split_interface = new SplitPane();
+        split_interface.setOrientation(Orientation.VERTICAL);
+        BorderPane bp1 = new BorderPane();
+        BorderPane bp2 = new BorderPane();
+        bp1.setTop(new Slider());
+        bp2.setBottom(new Slider());
+        bp1.setCenter(new Button());
+        bp2.setTop(new Button());
+        split_interface.getItems().addAll(bp1, bp2);
+        workspace.setCenter(split_interface);
+
     }
 
 
