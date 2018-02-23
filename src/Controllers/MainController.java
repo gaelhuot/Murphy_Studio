50727d77c0d137803ae6856fac213fd19d4a7d92
ThibaudJeannin <thibaud.jeannin@gmail.com>
2018-02-23 23:56:05 +0100

    Déplacé menu options/options

--- src/Controllers/MainController.java
+++ src/Controllers/MainController.java
@@ -40,6 +40,9 @@ public class MainController extends Controller
     public MenuItem menu_edit_redo;
     public MenuItem menu_view_set_light_theme;
     public MenuItem menu_view_set_dark_theme;
+    public MenuItem menu_help_about;
+    public MenuItem menu_options_options;
+    public MenuItem menu_options_options;
 
     public Slider master_volume_slider;
     public TextField sequencer_tempo;
@@ -49,8 +52,6 @@ public class MainController extends Controller
 
     public SplitPane splitPaneHorizontal;
     public SplitPane splitPaneVertical;
-    public MenuItem menu_help_about;
-    public MenuItem menu_edit_options;
 
     private Stage popup;
     private Pane popup_container;
@@ -87,7 +88,7 @@ public class MainController extends Controller
         menu_file_quit.setOnAction(e -> exit());
         menu_edit_undo.setOnAction(e -> System.out.println("Undo"));
         menu_edit_redo.setOnAction(e -> System.out.println("Redo"));
-        menu_edit_options.setOnAction(e -> setPopup("options.fxml", 600, 400));
+        menu_options_options.setOnAction(e -> setPopup("options.fxml", 600, 400));
         menu_view_set_dark_theme.setOnAction(e -> {this.setDarkTheme();});
         menu_view_set_light_theme.setOnAction(e -> {this.setLightTheme();});
         menu_help_about.setOnAction(e -> setPopup("about.fxml", 360, 250));
